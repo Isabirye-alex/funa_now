16798813346b7908e4c311198f855a95bef4126b
Isabirye-alex <alexisabirye558@gmail.com>
2025-07-24 19:45:56 +0300

    me

--- lib/ui/pages/stand_alone/loading_shimmer.dart
+++ lib/ui/pages/stand_alone/loading_shimmer.dart
@@ -1,69 +1,121 @@
 import 'package:flutter/material.dart';
 import 'package:shimmer/shimmer.dart';
 
-class LandingPageShimmer extends StatelessWidget {
+class LandingPageShimmer extends StatefulWidget {
   const LandingPageShimmer({super.key});
 
   @override
+  State<LandingPageShimmer> createState() => _LandingPageShimmerState();
+}
+
+class _LandingPageShimmerState extends State<LandingPageShimmer>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _pulse;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(seconds: 2),
+      vsync: this,
+    )..repeat(reverse: true);
+
+    _pulse = Tween<double>(
+      begin: 0.4,
+      end: 1.0,
+    ).animate(CurvedAnimation(parent: _controller, curve: Curves.easeInOut));
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Shimmer.fromColors(
-      baseColor: Colors.white,
-      highlightColor: const Color.fromARGB(255, 193, 218, 238),
-      child: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            children: List.generate(4, (index) {
-              return Container(
-                margin: const EdgeInsets.symmetric(vertical: 8),
-                padding: const EdgeInsets.all(12),
-                height: 140,
-                decoration: BoxDecoration(
-                  color: Colors.grey[300],
-                  borderRadius: BorderRadius.circular(16),
-                ),
-                child: Row(
-                  children: [
-                    Container(
-                      width: 100,
-                      height: 100,
-                      decoration: BoxDecoration(
-                        color: Colors.grey[400],
-                        borderRadius: BorderRadius.circular(12),
-                      ),
+    return Stack(
+      children: [
+        Shimmer.fromColors(
+          baseColor: Colors.white,
+          highlightColor: const Color.fromARGB(255, 193, 218, 238),
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: EdgeInsets.zero,
+              child: Column(
+                children: List.generate(1, (index) {
+                  return Container(
+                    height: MediaQuery.of(context).size.height,
+                    decoration: BoxDecoration(color: Colors.grey[300]),
+                    child: Row(
+                      children: [
+                        Container(
+                          width: 100,
+                          height: 100,
+                          color: Colors.grey[400],
+                        ),
+                        const SizedBox(width: 12),
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Container(
+                                height: 16,
+                                width: double.infinity,
+                                color: Colors.grey[400],
+                              ),
+                              const SizedBox(height: 8),
+                              Container(
+                                height: 14,
+                                width: 150,
+                                color: Colors.grey[400],
+                              ),
+                              const SizedBox(height: 8),
+                              Container(
+                                height: 14,
+                                width: 100,
+                                color: Colors.grey[400],
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
                     ),
-                    const SizedBox(width: 12),
-                    Expanded(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Container(
-                            height: 16,
-                            width: double.infinity,
-                            color: Colors.grey[400],
-                          ),
-                          const SizedBox(height: 8),
-                          Container(
-                            height: 14,
-                            width: 150,
-                            color: Colors.grey[400],
-                          ),
-                          const SizedBox(height: 8),
-                          Container(
-                            height: 14,
-                            width: 100,
-                            color: Colors.grey[400],
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              );
-            }),
+                  );
+                }),
+              ),
+            ),
           ),
         ),
-      ),
+        Positioned.fill(
+          child: Center(
+            child: FadeTransition(
+              opacity: _pulse,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: const [
+                  Icon(
+                    Icons.hourglass_top,
+                    size: 48,
+                    color: Colors.purpleAccent,
+                  ),
+                  SizedBox(height: 12),
+                  Text(
+                    "Hang tight! We're loading your experience...",
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.purpleAccent,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
